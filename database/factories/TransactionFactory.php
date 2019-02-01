2bd34a0eee8ff5c509f6268b1ae4b556ead154dd
Sheng Kai Teo <skaiteo98@gmail.com>
2019-02-02 00:29:20 +0800

    changed seeds for better tracking

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Transaction::class, function (Faker $faker) {
     return [
-        'passport_id' => rand(1, 1000),
-        'sender_id' => rand(1, 1000),
-        'receiver_id' => rand(1, 1000),
+        'passport_id' => rand(1, 20),
+        'sender_id' => rand(1, 5),
+        'receiver_id' => rand(1, 5),
         'received' => rand(0, 1),
-        'attachments' => $faker->sentence(6)
+        'attachments' => $faker->sentence(4)
     ];
 });
