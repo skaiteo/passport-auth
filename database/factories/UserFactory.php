57f575d8a9cddda93e246d7b0bc6a385b2f88d9c
Sheng Kai Teo <skaiteo98@gmail.com>
2019-02-02 12:29:39 +0800

    added faker master_code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'phone_number' => '01' . $faker->randomNumber(8, true),
         'd_o_b' => $faker->date,
+        'master_code' => str_random(6),
         'remember_token' => str_random(10),
     ];
 });
