2e7968821cc62a4a4ddaed5b8bb334d6207f3db4
Sheng Kai Teo <skaiteo98@gmail.com>
2019-02-13 11:19:44 +0800

    limit rand user_id to 5 users

--- database/factories/PassportFactory.php
+++ database/factories/PassportFactory.php
@@ -11,6 +11,6 @@ $factory->define(App\Passport::class, function (Faker $faker) {
         "d_o_b" => $faker->date('Y-m-d', '915148800'),
         "gender" => rand(0, 1) ? 'MALE' : 'FEMALE',
         "expiry_date" => $faker->date,
-        "user_id" => rand(1, 20)
+        "user_id" => rand(1, 5)
     ];
 });
