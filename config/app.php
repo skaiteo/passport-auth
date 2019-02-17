dd1c02c0d97fb77ce435b0a5f0e11f44c6599aae
Sheng Kai Teo <skaiteo98@gmail.com>
2019-02-17 14:00:07 +0800

    added cloudder

--- config/app.php
+++ config/app.php
@@ -173,7 +173,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
-        
+        JD\Cloudder\CloudderServiceProvider::class
     ],
 
     /*
@@ -222,7 +222,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Cloudder' => JD\Cloudder\Facades\Cloudder::class
     ],
 
 ];
