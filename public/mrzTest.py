506a253d7aa62a50a4c14c6e56074ac7970f811a
Sheng Kai Teo <skaiteo98@gmail.com>
2019-02-21 14:45:57 +0800

    return url

--- public/mrzTest.py
+++ public/mrzTest.py
@@ -4,19 +4,18 @@ from passporteye import read_mrz
 
 #Obtain Image from the argument
 image_file = sys.argv[1]
-# image_file = open(image_name)
-# print(image_file)
+print(image_file)
 #Pass image into Read_MRZ functionality and store inside MRZ Variable
-mrz = read_mrz(image_file)
+# mrz = read_mrz(image_file)
 
-#Change the data into dictionary of series or list like data type,
-#Similar to JSON but not really, then store inside MRZ_Data variable
-mrz_data = mrz.to_dict()
+# #Change the data into dictionary of series or list like data type,
+# #Similar to JSON but not really, then store inside MRZ_Data variable
+# mrz_data = mrz.to_dict()
 
-#Change the format of MRZ_DATA into JSON Format 
-jsonvalue = json.dumps(mrz_data)
+# #Change the format of MRZ_DATA into JSON Format 
+# jsonvalue = json.dumps(mrz_data)
 
-#Prints the JSON VALUE for PHP
-print(jsonvalue)
+# #Prints the JSON VALUE for PHP
+# print(jsonvalue)
 
 #I didnt use return because non of this code is inside a class or a method
